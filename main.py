9a41ba5378da166725a508ce94bc5cb36f3b16ef
Kejuan Harrison <bamaboi912@icloud.com>
2023-03-13 11:48:43 -0500

    establish connection from main.py to mysql

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
-# This is a sample Python script.
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import mysql.connector
+from mysql.connector import Error
+
+def create_server_connection(host_name, user_name, user_password):
+    connection = None
+    try:
+        connection = mysql.connector.connect(
+            host = host_name,
+            user = user_name,
+            passwd = user_password
+        )
+        print("MySQL Database Connection Successful")
+    except Error as err:
+        print(f"Error {err}")
+    return connection
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+#calling statement
+connection = create_server_connection("localhost", "root", "student")
--- readme.txt
+++ readme.txt
@@ -16,3 +16,28 @@ file>settings>name of project > project interpreter, search each extension then
 install
 database un: root pw:student
 screenshot of test connection after database
+
+Establish communication to mysql
+
+
+1.) Content for main.py
+
+import mysql.connector
+from mysql.connector import Error
+
+def create_server_connection(host_name, user_name, user_password):
+    connection = None
+    try:
+        connection = mysql.connector.connect(
+            host = host_name,
+            user = user_name,
+            passwd = user_password
+        )
+        print("MySQL Database Connection Successful")
+    except Error as err:
+        print(f"Error {err}")
+    return connection
+
+
+#calling statement
+connection = create_server_connection("localhost", "root", "student")
